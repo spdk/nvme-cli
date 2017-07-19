f50fcac91efcae89ea8ed2f11ed6c330bc502c71
GangCao <gang.cao@intel.com>
2017-07-19 19:07:23 -0400

    nvme-cli/spdk: add a bdf check function for input device
    Change-Id: If38be7a079fe152aed0205bad8824cff29cf3687
    Signed-off-by: GangCao <gang.cao@intel.com>

--- spdk-nvme.c
+++ spdk-nvme.c
@@ -115,6 +115,7 @@ attach_cb(void *cb_ctx, const struct spdk_nvme_transport_id *trid,
 			g_spdk_dev[g_num_ctrlr].fd = socket(AF_UNIX, SOCK_RAW, 0);
 			g_spdk_dev[g_num_ctrlr].io_qpair = io_qpair;
 			g_spdk_dev[g_num_ctrlr].ns_id = i;
+			snprintf(g_spdk_dev[g_num_ctrlr].traddr, sizeof(trid->traddr), "%s", trid->traddr);
 			if (trid->trtype == SPDK_NVME_TRANSPORT_RDMA) {
 				snprintf(g_spdk_dev[g_num_ctrlr].subnqn, sizeof(trid->subnqn), "%s", trid->subnqn);
 			}
@@ -178,6 +179,17 @@ spdk_unsupported_cmd(char *cmd_str)
 	return false;
 }
 
+static bool
+nvme_spdk_is_bdf_dev(char *str)
+{
+	/* The format is DDDD:BB:DD.FF */
+	if (strchr(str, ':')) {
+		return true;
+	}
+
+	return false;
+}
+
 static int
 spdk_parse_args(int argc, char **argv, bool *probe)
 {
@@ -201,9 +213,15 @@ spdk_parse_args(int argc, char **argv, bool *probe)
 		return 0;
 	}
 
-	snprintf(g_spdk_dev[g_num_ctrlr].traddr, SPDK_TRADDR_MAX_LEN, "%s", argv[1]);
+	for (int i = 1; i < argc; i++) {
+		if (nvme_spdk_is_bdf_dev(argv[i])) {
+			snprintf(g_spdk_dev[g_num_ctrlr].traddr, SPDK_TRADDR_MAX_LEN, "%s", argv[i]);
+			return 0;
+		}
+	}
 
-	return 0;
+	fprintf(stderr, "device information needed, example: 0000:07:00.0\n");
+	return 1;
 }
 
 int
