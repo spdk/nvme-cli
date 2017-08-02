cbd08700491133a7b8530c2f0ba13808de063b03
GangCao <gang.cao@intel.com>
2017-08-02 16:32:50 -0400

    nvme-cli/spdk: support more NVMe-oF configuration
    With this patch, it can support more NVMe-oF configurable
    parameters like hostnqn, nr_io_requests, keep_alive_tmo
    and so on.
    
    Change-Id: Ibd392341efc33a92b0e23a3e0ed518ab668b95db
    Signed-off-by: CaoGang <gang.cao@intel.com>

--- fabrics.h
+++ fabrics.h
@@ -1,6 +1,22 @@
 #ifndef _DISCOVER_H
 #define _DISCOVER_H
 
+struct config {
+	char *nqn;
+	char *transport;
+	char *traddr;
+	char *trsvcid;
+	char *host_traddr;
+	char *hostnqn;
+	char *hostid;
+	char *nr_io_queues;
+	char *queue_size;
+	char *keep_alive_tmo;
+	char *reconnect_delay;
+	char *raw;
+	char *device;
+};
+
 extern int fdiscover(const char *desc, int argc, char **argv, bool connect);
 extern int fconnect(const char *desc, int argc, char **argv);
 extern int fdisconnect(const char *desc, int argc, char **argv);
