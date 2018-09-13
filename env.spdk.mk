23ef4147c6d1f5c0b8c5300dc7ba7bd9ac6c8a8c
Ziye Yang <optimistyzy@gmail.com>
2018-09-13 20:24:33 +0800

    Update env.spdk.mk
    Purpose: To add a new library spdk_sock into the link.
    
    Reason: We will support TCP/IP transport in NVMe
    driver in SPDK. And without this lib, there
    will be compilation error.
    
    Change-Id: Id6e9c916b227cc63a24cfcca6bf7398a243951e1
    Signed-off-by: Ziye Yang <optimistyzy@gmail.com>

--- env.spdk.mk
+++ env.spdk.mk
@@ -41,7 +41,7 @@ include $(SPDK_ROOT_DIR)/CONFIG
 override CFLAGS += -I$(SPDK_ROOT_DIR)/include
 override LDFLAGS += \
 	-Wl,--whole-archive \
-	-L$(SPDK_LIB_DIR) -lspdk_log -lspdk_nvme -lspdk_env_dpdk -lspdk_util \
+	-L$(SPDK_LIB_DIR) -lspdk_log -lspdk_sock -lspdk_nvme -lspdk_env_dpdk -lspdk_util \
 	-L$(DPDK_LIB_DIR) -lrte_eal -lrte_mempool -lrte_ring -lrte_pci -lrte_bus_pci \
 	-Wl,--no-whole-archive \
 	-ldl -pthread -lrt -lrdmacm -lnuma -libverbs
