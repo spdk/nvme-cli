d8d2450532f144c6d46a69f803b32fbd6985e44a
Darek Stojaczyk <dariusz.stojaczyk@intel.com>
2018-09-17 11:58:27 +0200

    nvme-cli/spdk: link with rte_kvargs if present
    Starting with DPDK 18.08, rte_kvargs is a dependency
    of rte_eal.
    
    Change-Id: I603d7ea54481f952ba1f9c2ced62c6ca1daa095b
    Signed-off-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>

--- env.spdk.mk
+++ env.spdk.mk
@@ -37,12 +37,17 @@ SPDK_LIB_DIR ?= $(SPDK_ROOT_DIR)/build/lib
 include $(SPDK_ROOT_DIR)/mk/config.mk
 
 DPDK_LIB_DIR ?= $(CONFIG_DPDK_DIR)/lib
+DPDK_LIB_LIST = -lrte_eal -lrte_mempool -lrte_ring -lrte_pci -lrte_bus_pci
+
+ifneq (, $(wildcard $(DPDK_LIB_DIR)/librte_kvargs.*))
+DPDK_LIB_LIST += -lrte_kvargs
+endif
 
 override CFLAGS += -I$(SPDK_ROOT_DIR)/include
 override LDFLAGS += \
 	-Wl,--whole-archive \
 	-L$(SPDK_LIB_DIR) -lspdk_log -lspdk_nvme -lspdk_env_dpdk -lspdk_util \
-	-L$(DPDK_LIB_DIR) -lrte_eal -lrte_mempool -lrte_ring -lrte_pci -lrte_bus_pci \
+	-L$(DPDK_LIB_DIR) $(DPDK_LIB_LIST) \
 	-Wl,--no-whole-archive \
 	-ldl -pthread -lrt -lrdmacm -lnuma -libverbs
 
